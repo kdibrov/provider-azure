0789832af2663115577ffddde66fb8d624cd62c7
kdibrov <kdibrov@gmail.com>
2023-02-15 16:21:58 +0100

    Adding sql(13) resources

--- internal/controller/zz_setup.go
+++ internal/controller/zz_setup.go
@@ -585,8 +585,12 @@ import (
 	cloudapplicationliveview "github.com/upbound/provider-azure/internal/controller/spring/cloudapplicationliveview"
 	mssqldatabase "github.com/upbound/provider-azure/internal/controller/sql/mssqldatabase"
 	mssqldatabaseextendedauditingpolicy "github.com/upbound/provider-azure/internal/controller/sql/mssqldatabaseextendedauditingpolicy"
+	mssqldatabasevulnerabilityassessmentrulebaseline "github.com/upbound/provider-azure/internal/controller/sql/mssqldatabasevulnerabilityassessmentrulebaseline"
+	mssqlelasticpool "github.com/upbound/provider-azure/internal/controller/sql/mssqlelasticpool"
 	mssqlfailovergroup "github.com/upbound/provider-azure/internal/controller/sql/mssqlfailovergroup"
 	mssqlfirewallrule "github.com/upbound/provider-azure/internal/controller/sql/mssqlfirewallrule"
+	mssqljobagent "github.com/upbound/provider-azure/internal/controller/sql/mssqljobagent"
+	mssqljobcredential "github.com/upbound/provider-azure/internal/controller/sql/mssqljobcredential"
 	mssqlmanageddatabase "github.com/upbound/provider-azure/internal/controller/sql/mssqlmanageddatabase"
 	mssqlmanagedinstance "github.com/upbound/provider-azure/internal/controller/sql/mssqlmanagedinstance"
 	mssqlmanagedinstanceactivedirectoryadministrator "github.com/upbound/provider-azure/internal/controller/sql/mssqlmanagedinstanceactivedirectoryadministrator"
@@ -598,6 +602,7 @@ import (
 	mssqlservermicrosoftsupportauditingpolicy "github.com/upbound/provider-azure/internal/controller/sql/mssqlservermicrosoftsupportauditingpolicy"
 	mssqlserversecurityalertpolicy "github.com/upbound/provider-azure/internal/controller/sql/mssqlserversecurityalertpolicy"
 	mssqlservertransparentdataencryption "github.com/upbound/provider-azure/internal/controller/sql/mssqlservertransparentdataencryption"
+	mssqlservervulnerabilityassessment "github.com/upbound/provider-azure/internal/controller/sql/mssqlservervulnerabilityassessment"
 	mssqlvirtualnetworkrule "github.com/upbound/provider-azure/internal/controller/sql/mssqlvirtualnetworkrule"
 	accountstorage "github.com/upbound/provider-azure/internal/controller/storage/account"
 	accountnetworkrules "github.com/upbound/provider-azure/internal/controller/storage/accountnetworkrules"
@@ -1260,8 +1265,12 @@ func Setup(mgr ctrl.Manager, o controller.Options) error {
 		cloudapplicationliveview.Setup,
 		mssqldatabase.Setup,
 		mssqldatabaseextendedauditingpolicy.Setup,
+		mssqldatabasevulnerabilityassessmentrulebaseline.Setup,
+		mssqlelasticpool.Setup,
 		mssqlfailovergroup.Setup,
 		mssqlfirewallrule.Setup,
+		mssqljobagent.Setup,
+		mssqljobcredential.Setup,
 		mssqlmanageddatabase.Setup,
 		mssqlmanagedinstance.Setup,
 		mssqlmanagedinstanceactivedirectoryadministrator.Setup,
@@ -1273,6 +1282,7 @@ func Setup(mgr ctrl.Manager, o controller.Options) error {
 		mssqlservermicrosoftsupportauditingpolicy.Setup,
 		mssqlserversecurityalertpolicy.Setup,
 		mssqlservertransparentdataencryption.Setup,
+		mssqlservervulnerabilityassessment.Setup,
 		mssqlvirtualnetworkrule.Setup,
 		accountstorage.Setup,
 		accountnetworkrules.Setup,
